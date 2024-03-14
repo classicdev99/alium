f182c2ef91b36593c7c24d58fdcb5f9cf4c593e3
ClassicDev <classicdev99@gmail.com>
2024-03-13 22:05:03 -0700

    first commit

---
+++ _next/static/chunks/pages/pools-91546c33af26f55db4ff.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6551],{22485:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/pools",function(){return u(91446)}])}},function(n){n.O(0,[588,4343,7676,4408,8687,4049,7409,5892,8752,4035,7521,1446,9774,2888,179],(function(){return _=22485,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
